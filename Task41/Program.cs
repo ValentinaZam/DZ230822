b751f17bccefc8d94b74abb878c598d75b07621c
Валентина <abc99-33@yandex.ru>
2022-08-24 22:33:34 +0300

    Task41,43

---
+++ Task41/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.WriteLine("Сколько чисел Вы хотите ввести: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int count = 0;
+int [] array = new int[num];
+for (int i = 0; i < num; i++)
+{
+  Console.WriteLine($"Введите число {i + 1}: ");
+  array[i] = Convert.ToInt32(Console.ReadLine());
+
+  if (array[i] > 0) count = count + 1;
+}
+Console.WriteLine($"Количество положительных чисел: {count}");
+
---
+++ Task43/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 43: Напишите программу, 
+// которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+
+// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+Console.WriteLine("Введите b1: ");
+int b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите k1: ");
+int k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите b2: ");
+int b2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите k2: ");
+int k2 = Convert.ToInt32(Console.ReadLine());
+if (k1 == k2) {
+  Console.WriteLine("ERROR: <делениe на ноль> Введите другие данные! K1 не должно совпадать с K2 ");
+}
+else 
+{
+double x = (double) (b2 - b1) / (k1 - k2);
+double y = k2 * x + b2;
+Console.WriteLine($"Точка пересечения: ({x} ; {y})");
+}
